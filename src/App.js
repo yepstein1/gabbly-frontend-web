074611b0bf9c34b620cb4ddca879fd0357246d61
yaakovepstein <yaakovesptein@gmail.com>
2021-04-24 22:47:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {useState} from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [text, setText] = useState("")
+    const [from, setFrom] = useState("")
+    const [to, setTo] = useState("")
+    const [language, setLanguage] = useState("")
+
+    return (
+        <div className="App">
+            <label for="textmessage">Enter Text you want to send</label> &nbsp;
+            <input type="text" class="textmessage" value={text} onChange={(e) => setText(e.target.value)}></input>
+            <br/>
+            <label for="fromNumber"> Enter Number you want to send message from</label> &nbsp;
+            <input type="text" class="fromNumber" value={from} onChange={(e) => setFrom(e.target.value)}></input>
+
+            <br/>
+            <label for="toNumber">Enter Number you want message sent to</label> &nbsp;
+            <input type="text" class="toNumber" value={to} onChange={(e) => setTo(e.target.value)}></input>
+            <br/>
+            <label for="languageSelect">Select language</label> &nbsp;
+            <select class="languageSelect" onChange={(e) => setLanguage(e.target.value)}>
+                <option value="fr">French</option>
+                <option value="es">Spanish</option>
+                <option value="ar">Arabic</option>
+                <option value="he">Hebrew</option>
+            </select>
+
+            <button onClick={() => {
+                const data = {
+                    'text': text,
+                    'from': from,
+                    'to': to,
+                    'language': language
+                }
+                fetch("https://d034nfeoml.execute-api.us-east-1.amazonaws.com/myStage/sam-app-helloFromLambdaFunction-1W88HQIV5IN",
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    }
+                ).then(response => response.json()).then(data => {
+                    console.log('Success:', data);
+                })
+                    .catch((error) => {
+                        console.error('Error:', error);
+                    })
+
+
+            }
+
+            }>Send and translate
+            </button>
+
+
+        </div>
   );
 }
 
